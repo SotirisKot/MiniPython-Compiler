537fcb1027c3a2aa7827cf1b590d53a9dc27cbc1
Sotiris <sotiriskot9@gmail.com>
2018-12-26 12:27:17 +0200

    variables test now available

---
+++ variables_test.py
@@ -0,0 +1,24 @@
+def foo():
+    print x
+    
+def pepe(y):
+    return s
+
+k = 2
+def lol():
+    print k
+
+assert z == k
+z = 1
+a=2
+
+for hel in v:
+    print hel
+print hel
+
+for goo in z:
+    print goo
+print goo
+
+while y > a:
+    print z
